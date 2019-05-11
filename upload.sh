25a89717511e49789dd7433c43c0dccf0340d6c1
0xFE <mohit@muthanna.com>
2019-05-11 13:56:08 -0400

    Cache images forever.

--- upload.sh
+++ upload.sh
@@ -4,4 +4,5 @@ BUCKET=gs://arkin.ninja
 URL=${BUCKET}
 
 echo Uploading website...
-gsutil -h "Cache-control:public,max-age=300" -m cp -a public-read -z html,css,js -r * ${URL}
+gsutil -h "Cache-control:public,max-age=300" -m cp -a public-read -z html,css,js index.* ${URL}
+gsutil -h "Cache-control:public,max-age=31536000" -m cp -a public-read -r pages ${URL}
